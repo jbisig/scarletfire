bb81a4ec07232024cf9ee44fb9dbeae860d4aca9
Jesse Bisignano <93935124+jbisig@users.noreply.github.com>
2026-04-10 22:39:29 -0700

    Bump version to 1.2.2

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default ({ config }) => {
       name: "Scarlet>fire",
       slug: "scarletfire",
       owner: "scarlet-fire",
-      version: "1.2.1",
+      version: "1.2.2",
       orientation: "portrait",
       icon: "./assets/icon.png",
       userInterfaceStyle: "dark",
